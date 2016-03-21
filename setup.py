a0512fd3ccc80b05daeb0dbc947b25e1c49cf65c
Atsushi Suga <a2csuga@users.noreply.github.com>
2016-03-22 00:38:35 +0900

    lektor_root_relative_path is moved from file to directory and __init__.py

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 setup(
     name='lektor-root-relative-path',
-    version='0.1',
     author=u'Atsushi Suga',
-    author_email='',
+    author_email='a2csuga@users.noreply.github.com',
+    version='0.1',
+    url='http://github.com/a2csuga/lektor-root-relative-path',
     license='MIT',
-    py_modules=['lektor_root_relative_path'],
+    packages=['lektor_root_relative_path'],
+    description='Root relative path plugin for Lektor',
     entry_points={
         'lektor.plugins': [
             'root-relative-path = lektor_root_relative_path:RootRelativePathPlugin',
