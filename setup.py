5c200bdae50a78a2a295a3c0bb5440004b7fa72a
Atsushi Suga <a2csuga@users.noreply.github.com>
2018-09-24 10:13:28 +0900

    Version is up to 0.2.1
    The module file __init__.py is move to lektor_root_relative_path.py
    Some modification in setup.py
    Add attributes in classifiers to follow the guide in lektor website

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import io
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with io.open('README.md', 'rt', encoding="utf8") as f:
     readme = f.read()
@@ -8,15 +8,18 @@ setup(
     name='lektor-root-relative-path',
     author=u'Atsushi Suga',
     author_email='a2csuga@users.noreply.github.com',
-    version='0.2.0',
+    version='0.2.1',
     classifiers=[
         'Framework :: Lektor',
+        'Environment :: Web Environment',
         'Environment :: Plugins',
+        'License :: OSI Approved :: MIT License',
     ],
     url='http://github.com/a2csuga/lektor-root-relative-path',
     license='MIT',
     install_requires=open('requirements.txt').read(),
-    packages=['lektor_root_relative_path'],
+    packages=find_packages(),
+    py_modules=['lektor_root_relative_path'],
     description='Root relative path plugin for Lektor',
     long_description=readme,
     long_description_content_type='text/markdown',
